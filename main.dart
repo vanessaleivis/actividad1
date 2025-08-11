b5b9d6335fabe8e46a672457dcae8987f08e643a
unknown <vanessaleivis7>
2025-08-11 07:15:04 -0500

    segundo comit

--- main.dart
+++ main.dart
@@ -5,107 +5,99 @@ import 'dart:math';
 void main() {
   //Ejercicios para empezar la movile
 
-   
-  print("1 calcula tu distancia");
+  while (true) {
+    print("1 calcula tu distancia");
   print("2 chismea tus notas");
   print("3 averigue sus partidos ganados");
   print("4 datos de empleados");
   print("5 averigue el triangulo");
   print("6  transforme su temperatura");
+  print("7 salir");
 
   print("escoja una de las opciones que valla a realizar: ");
   int? opcion = int.tryParse(stdin.readLineSync()!);
 
-  int suma(int nota1, int nota2, int nota3) {
-        return nota1 + nota2 + nota3;
-      }
 
 
   switch (opcion) {
     case 1:
       print("ingrese la velocidad en m/s");
-      double? velocidad = double.parse(stdin.readLineSync()!);
+      double? velocidad = double.tryParse(stdin.readLineSync()!);
 
       print("ingrese el tiempo en segundos");
-      double? tiempo = double.parse(stdin.readLineSync()!);
+      double? tiempo = double.tryParse(stdin.readLineSync()!);
 
-      double distancia = velocidad * tiempo;
-
-      print("la distancia recorrida es $distancia metros.");
+      double result = distancia(velocidad!,tiempo!);
+      print("la distancia recorrida es $result");
+      print('');
       break;
+
     case 2:
       print('ingrese su primera nota');
-      int? nota1 = int.tryParse(stdin.readLineSync()!);
+      double? nota1 = double.tryParse(stdin.readLineSync()!);
       print('ingrese su segunda nota');
-      int? nota2 = int.tryParse(stdin.readLineSync()!);
+      double? nota2 = double.tryParse(stdin.readLineSync()!);
       print('ingrese su tercera nota');
-      int? nota3 = int.tryParse(stdin.readLineSync()!);
+      double? nota3 = double.tryParse(stdin.readLineSync()!);
+      
 
       
-      int result = suma(nota1!, nota2!, nota3!);
-      double promedio = result / 3;
-      print('Este es tu: $promedio');
+      double result = suma(nota1!, nota2!, nota3!); 
+      print('Este es tu: $result');
 
       if (nota1 < nota2 && nota3 >= nota1) {
         print("perdiste el semestre manit@");
       } else {
-        print("felicidades manito tu promedio es: $promedio ");
+        print("felicidades manito tu promedio es: $result ");
       }
       break;
     case 3:
       print("ingrese partidos ganados ");
-      int? perdidos = int.tryParse(stdin.readLineSync()!);
-
-      print("ingrese partidos ganados");
       int? ganados = int.tryParse(stdin.readLineSync()!);
 
       print("ingrese partidos empatados");
       int? empatados = int.tryParse(stdin.readLineSync()!);
 
-      int puntos = 0;
+      print("ingrese partidos perdidos");
+      int? perdidos = int.tryParse(stdin.readLineSync()!);
 
-      if(ganados! > 0){
-        puntos += ganados * 3;
-      }else if (empatados! > 0){
-        puntos += empatados * 1;
-      }else{
-        print("no tienes puntos");
-      }
+      int result = puntos (ganados!,empatados!,perdidos!);
 
-    print("partidos ganados: $ganados, partidos perdidos: $perdidos, partidos empatados: $empatados");
-      print("total de puntaje : $puntos");
+
+
+    print("partidos ganados: $ganados , partidos empatados: $empatados, partidos perdidos: $perdidos");
+      print("total de puntaje : $result");
+      print('');
+
       break;
     case 4:
       print("nombre empleado");
       String? nombre = stdin.readLineSync();
 
       print("cantidad de horas laboradas en el mes ");
-      int? horas = int.tryParse(stdin.readLineSync()!);
+      double? horas = double.tryParse(stdin.readLineSync()!);
 
       print("tarifa horas");
-      int? tarifas = int.tryParse(stdin.readLineSync()!);
+      double? tarifas = double.tryParse(stdin.readLineSync()!);
 
-      double devengado = horas!.toDouble() * tarifas!.toDouble();
+      double result = devengado (horas!,tarifas!);
+      print("tu platica es $result");
+      print('');
 
-
-      print("n\Plantilla");
-      print("Nombre: \$nombre");
-      print("horas laboradas: \$horas");
-      print("tarifas: \$tarifas");
-      print("Total devengado: \$\totaldevengado");
 
 
       break;
     case 5:
       print("ingrese el valor de a");
-      int? a = int.tryParse(stdin.readLineSync()!);
+      double? a = double.tryParse(stdin.readLineSync()!);
 
       print("ingrese el valor de b");
-      int? b = int.tryParse(stdin.readLineSync()!);
+      double? b = double.tryParse(stdin.readLineSync()!);
 
-      double h = sqrt(pow(a!, 2) + pow(b!, 2));
+      double result = hipotenusa(a!, b!, );
+      sqrt(result);
 
-      print("la hipotenusa del triangulo es : $h"); 
+      print("la hipotenusa del triangulo es : $result"); 
 
 
       break;
@@ -113,18 +105,42 @@ void main() {
       print("ingrese la temperatura en grados celsius");
       double? celsius = double.tryParse(stdin.readLineSync()!);
 
-      double fahrenheit = (celsius! * 1.8) + 32;
+      double result = fahrenheit(celsius!);
 
-      print("la temperatura en fahrenheit es: $fahrenheit °F");
+      print("la temperatura en fahrenheit es: $result °F");
 
       break;  
+    case 7:
+      int? salir = int.tryParse(stdin.readLineSync()!);
+    break;
     default:
   }
+    
+  }
+  
+  
+}
+double distancia(double velocidad, double tiempo){
+  return (velocidad * tiempo);
 }
 
+double suma(double nota1,double nota2,double nota3){
+  return (nota1+nota2+nota3)/3;
+}
+int puntos(int ganados,int empatados,int perdidos){
+  return(ganados * 3)+(empatados * 1)+(perdidos * 0);
+}
 
+double devengado(double horas, double tarifas){
+  return(horas * tarifas);
+}
+double hipotenusa(double a, double b){
+  return sqrt(pow(a, 2) + pow(b, 2));
+}
+double fahrenheit(double celsius){
+  return (celsius * 1.8) + 32;
 
-
+}
 
 
 
